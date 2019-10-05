6b383cc321eaeac862ef753235ad6a2000eae55f
NogNoa <53447424+NogNoa@users.noreply.github.com>
2019-10-05 17:56:22 +0300

    Add files via upload

--- Fundementals.py
+++ Fundementals.py
@@ -14,12 +14,15 @@ def Mtrx(L,n,m='same'):
             Mat[(i//m,i%m)]=L[i]
         except:
             Mat[(i//m,i%m)]=0
+    Mat['n'],Mat['m']=n,m
     return Mat
-def MtrxMltpl(A,B,n,m='same'):
+def MtrxMltpl(A,B,n='natural',m='natural'):
     #A, B should be matices in dictionary form
     #n number of lines, m number of rows, of desired product
-    if m=='same':
-        m=n
+    if n=='natural':
+        n=A['m']
+    if m=='natural':
+        m=B['n']
     Mat={}
     for i in range(n):
         for j in range(m):
@@ -29,6 +32,7 @@ def MtrxMltpl(A,B,n,m='same'):
                     Mat[(i,j)]+=A[(i,k)]*B[(k,j)]
                 except:
                     Mat[(i,j)]+=0
+    Mat['n'],Mat['m']=n,m
     return Mat
 def SclrMltpl(A,s):
     #A should be matix, s an integer
@@ -44,12 +48,21 @@ def IMtrx(n,s=1):
                 Mat[k,k]=s
             else:
                 Mat[i,k]=0
+    Mat['n'],Mat['m']=n,n
     return Mat
-        
+def PrntMtrx(A):
+    for i in range(A['n']):
+        p=''
+        for j in range(A['m']):
+            p+=str((A[i,j]))+',  '*(j!=A['m']-1)
+        print(p)
+    return
 A=Mtrx([1,2,3,4,5],2,3)
 B=Mtrx([2,0,0,2],2)
-C=MtrxMltpl(A,B,2)
-D=(SclrMltpl(A,2))
-E=MtrxMltpl(A,IMtrx(3,2),2,3)
-F=MtrxMltpl(IMtrx(3,2),IMtrx(3,2),3)
-print(F)
+C=MtrxMltpl(A,B)
+D=SclrMltpl(A,2)
+E=MtrxMltpl(A,IMtrx(3,2))
+F=MtrxMltpl(IMtrx(3,2),IMtrx(3,2))
+PrntMtrx(F)
+print('\n')
+PrntMtrx(E)
