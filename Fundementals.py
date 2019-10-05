b389cb9e59c045bab65ed5eeed6ffda19fbeef4e
NogNoa <53447424+NogNoa@users.noreply.github.com>
2019-10-05 16:40:08 +0300

    Add files via upload

--- Fundementals.py
+++ Fundementals.py
@@ -1,21 +1,39 @@
-def CnstrctםMtrx(L,n):
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  4 20:19:16 2019
+
+@author: omer
+"""
+def Mtrx(L,n,m):
     #L should be a list
     Mat={}
-    for i in range(n**2):
+    for i in range(n*m):
         try:
-            Mat[(i//n,i%n)]=L[i]
+            Mat[(i//m,i%m)]=L[i]
         except:
-            Mat[(i//n,i%n)]=0
+            Mat[(i//m,i%m)]=0
     return Mat
-def MtrxםMltpl(A,B):
+def MtrxMltpl(A,B,n,m):
     #A, B should be matices in dictionary form
     C={}
-    for i in range(2):
-        for j in range(2):
+    kull=min(n,m)
+    for i in range(n):
+        for j in range(m):
             C[(i,j)]=0
-            for k in range(2):
-                C[(i,j)]+=A[(i,k)]*B[(k,j)]
+            for k in range(kull):
+                try:
+                    C[(i,j)]+=A[(i,k)]*B[(k,j)]
+                except:
+                    C[(i,j)]+=0
     return C
-A=CnstrctםMtrx([1,2,3,4,5],3)
-B=CnstrctםMtrx([2,0,0,2],2)
-C=MtrxםMltpl(A,B)
+def SclrMltpl(A,s):
+    #A should be matix, s an integer
+    Scal={}
+    for i in A:
+        Scal[i]=A[i]*s
+    return Scal
+        
+A=Mtrx([1,2,3,4,5],2,3)
+B=Mtrx([2,0,0,2],2,2)
+C=MtrxMltpl(A,B,2,2)
+D=(SclrMltpl(A,2))
